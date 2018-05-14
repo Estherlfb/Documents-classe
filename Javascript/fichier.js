6658cffd5f272bb6e0771936812778a50db467c4
Esther Lefebvre <esther.lefebvre31@gmail.com>
2018-05-14 11:11:40 +0100

    Projet Javascript

---
+++ Javascript/fichier.js
@@ -0,0 +1,36 @@
+"use strict"
+
+// let message
+// message = "hello";
+// message = "bonjour";
+
+
+
+// let hello = "hello bonjour";
+// let message ="hello";
+
+// alert (hello);
+// alert (message);
+
+// let number = 431857545;
+// let string  = "21";
+
+// let boolean = true;
+// let boolean2 = false;
+
+// let x = null;
+// alert(x);
+
+// null
+// undefined
+
+// typeof number;
+// typeof string;
+
+// const user = "benoit";
+
+// alert(typeof user);
+
+// alert (Number(string));
+
+console.log (num2);
