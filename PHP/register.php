1afa67645f4c0c9c9cd5955cf3f66272db8e5c64
Esther Lefebvre <esther.lefebvre31@gmail.com>
2018-05-16 16:38:38 +0100

    2eme version avec Inscription et Connexion php

---
+++ PHP/register.php
@@ -0,0 +1,111 @@
+<!-- // FORMUALIRE D'INSCRIPTION  -->
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 3 | Registration Page</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="./dist/css/adminlte.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="./plugins/iCheck/square/blue.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="register-logo">
+    <a href="../../index2.html"><b>Admin</b>LTE</a>
+  </div>
+
+  <div class="card">
+    <div class="card-body register-card-body">
+      <p class="login-box-msg">Register a new membership</p>
+
+      <form action="./dash.php" method="post">
+        <div class="form-group has-feedback">
+          <input type="text" class="form-control" name="nom" placeholder="Full name">
+          <span class="fa fa-user form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="email" class="form-control" name="email" nplaceholder="Email">
+          <span class="fa fa-envelope form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="password" class="form-control" name="mdp1" placeholder="Password">
+          <span class="fa fa-lock form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="password" class="form-control" name="confirm_mdp1" placeholder="Retype password">
+          <span class="fa fa-lock form-control-feedback"></span>
+        </div>
+
+                <?php
+                if(isset($_GET["erreur"])){
+                  if($_GET["erreur"]==1){
+                    echo '<p style="color:#F60404;"> Les deux mots de passe sont différents. </p>'; 
+                  }
+                }
+
+                if(isset($_GET["erreurlogin"])){
+                    if($_GET["erreurlogin"]==2){
+                      echo '<p style="color:#F60404;"> Le login existe déjà. </p>'; 
+                    }
+                  }
+
+                  if(isset($_GET["erreurmail"])){
+                    if($_GET["erreurmail"]==3){
+                      echo '<p style="color:#F60404;"> L\'email existe déjà. </p>'; 
+                    }
+                  }
+                ?> 
+                
+
+        <div class="row">
+          <div class="col-8">
+            <div class="checkbox icheck">
+              <label>
+                <input type="checkbox"> I agree to the <a href="#">terms</a>
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <a href="index.php" class="text-center">I already have a membership</a>
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
+<!-- /.register-box -->
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- iCheck -->
+<script src="../../plugins/iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass   : 'iradio_square-blue',
+      increaseArea : '20%' // optional
+    })
+  })
+</script>
+</body>
+</html>
